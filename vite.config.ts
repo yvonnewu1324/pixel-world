171ae089301a4ccc7c04955325cca08121a87137
Yvonne Wu <yvonnewu1324@gmail.com>
2025-11-04 01:51:39 -0800

    remove server host

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  server: {
-    allowedHosts: ['qbosv-168-150-59-105.a.free.pinggy.link']
-  },
   plugins: [react()],
 })
